b63ad2c8b86b84bf17cbe952ab8cff5b49a3f3d1
Adnan Shaikh <adnan.shaikh1806@gmail.com>
2020-12-08 20:24:02 -0600

    Add Jupyer to list of deps

--- deps.ps1
+++ deps.ps1
@@ -92,6 +92,7 @@ code --install-extension GrapeCity.gc-excelviewer
 code --install-extension James-Yu.latex-workshop
 code --install-extension mechatroner.rainbow-csv
 code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
 code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.powershell
